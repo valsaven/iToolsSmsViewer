130fb854f7ebe6b4cb11d4679aa244166fe0eaab
Unknown <val.saven@gmail.com>
2016-10-26 14:38:21 +0300

    feat(main window): Resize; Add icon; Remove opening DevTools

--- main.js
+++ main.js
@@ -12,7 +12,11 @@ let win;
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600});
+  win = new BrowserWindow({
+    width: 320,
+    height: 480,
+    icon: 'sms.png',
+  });
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -20,9 +24,6 @@ function createWindow() {
     protocol: 'file:',
     slashes: true,
   }));
-
-  // Open the DevTools.
-  win.webContents.openDevTools();
 
   // Emitted when the window is closed.
   win.on('closed', () => {
