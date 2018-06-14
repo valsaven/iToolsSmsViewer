5e6cdf31eee0f1cf265af3761739d9b43e9c7845
Val Saven <val.saven@gmail.com>
2018-06-14 07:56:22 +0300

    Add linter for *.vue files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,23 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: 'babel-eslint',
     sourceType: 'module'
   },
   env: {
     browser: true,
     node: true
   },
-  extends: 'airbnb-base',
+  extends: [
+    'airbnb-base',
+    'plugin:vue/recommended'
+  ],
   globals: {
     __static: true
   },
   plugins: [
-    'html'
+    'vue'
   ],
   'rules': {
     'global-require': 0,
