c7fe563f1cf903b3d085c4c78d8fd69f149c715a
Val Saven <val.saven@gmail.com>
2023-06-17 22:58:41 +0300

    Add SQLite driver

--- build.sbt
+++ build.sbt
@@ -5,5 +5,6 @@ ThisBuild / scalaVersion := "3.3.0"
 lazy val root = (project in file("."))
   .settings(
     name := "iToolsSmsViewer",
-    libraryDependencies += "org.scalafx" %% "scalafx" % "20.0.0-R31"
+    libraryDependencies += "org.scalafx" %% "scalafx" % "20.0.0-R31",
+    libraryDependencies += "org.xerial" % "sqlite-jdbc" % "3.42.0.0"
   )
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -9,6 +9,8 @@ import scalafx.scene.paint.Color._
 import scalafx.scene.paint._
 import scalafx.scene.text.Text
 
+import java.sql.{Connection, DriverManager, ResultSet}
+
 object main extends JFXApp3 {
   override def start(): Unit = {
     stage = new JFXApp3.PrimaryStage {
@@ -21,8 +23,8 @@ object main extends JFXApp3 {
 
           children = Seq(
             new Text {
-              text = "iToolsSmsViewer"
-              style = "-fx-font: normal bold 100pt sans-serif"
+              text = fetchTextFromDatabase()
+              style = "-fx-font: normal bold 20pt sans-serif"
               fill = new LinearGradient(
                 endX = 0,
                 stops = Stops(Red, DarkRed))
@@ -30,7 +32,7 @@ object main extends JFXApp3 {
 
             new Text {
               text = "!!!"
-              style = "-fx-font: italic bold 100pt sans-serif"
+              style = "-fx-font: italic bold 20pt sans-serif"
               fill = new LinearGradient(
                 endX = 0,
                 stops = Stops(White, DarkGray)
@@ -45,5 +47,54 @@ object main extends JFXApp3 {
         }
       }
     }
+  }
+
+  def fetchTextFromDatabase(): String = {
+    Class.forName("org.sqlite.JDBC")
+
+    val connectionUrl = "jdbc:sqlite:./sms.db"
+    val connection: Connection = DriverManager.getConnection(connectionUrl)
+
+    var result = ""
+    try {
+      val statement = connection.createStatement()
+
+      val queryGetAll =
+        """
+        SELECT chat.ROWID as subscriber_id,
+        chat.chat_identifier AS number,
+        message.ROWID AS message_id,
+        message.date AS date,
+        message.text AS text,
+        message.is_from_me AS is_from_me
+        FROM chat_message_join
+        INNER JOIN chat
+        ON chat_message_join.chat_id = chat.ROWID
+        INNER JOIN message
+        ON chat_message_join.message_id = message.ROWID
+        ORDER BY subscriber_id, date ASC
+      """
+
+      val resultSet: ResultSet = statement.executeQuery(queryGetAll)
+
+      val stringBuilder = new StringBuilder
+
+      while (resultSet.next()) {
+        val subscriberId = resultSet.getString("subscriber_id")
+        val number = resultSet.getString("number")
+        val messageId = resultSet.getString("message_id")
+        val date = resultSet.getString("date")
+        val text = resultSet.getString("text")
+        val isFromMe = resultSet.getString("is_from_me")
+
+        stringBuilder.append(s"Subscriber ID: $subscriberId, Number: $number, Message ID: $messageId, Date: $date, Text: $text, Is From Me: $isFromMe\n")
+      }
+      result = stringBuilder.toString()
+    } catch {
+      case e: Exception => e.printStackTrace
+    } finally {
+      connection.close()
+    }
+    result
   }
 }
