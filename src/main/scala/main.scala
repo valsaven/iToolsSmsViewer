e518b4824a4751e96208b5a6af7eef3d7d8e7263
Val Saven <val.saven@gmail.com>
2023-06-19 15:30:29 +0300

    Add displaying data

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,64 +1,76 @@
 package i_tools_sms_viewer
 
-import scalafx.application.JFXApp3
-import scalafx.geometry.Insets
-import scalafx.scene.Scene
-import scalafx.scene.effect.DropShadow
-import scalafx.scene.layout.HBox
-import scalafx.scene.paint.Color._
-import scalafx.scene.paint._
-import scalafx.scene.text.Text
-
 import java.sql.{Connection, DriverManager, ResultSet}
+
+import java.time._
+import java.time.format.DateTimeFormatter
+
+import scalafx.application.JFXApp3
+import scalafx.scene.Scene
+import scalafx.scene.control._
+
+import scalafx.scene.control.cell.TextFieldListCell
+import scalafx.util.StringConverter
+import scalafx.Includes._
+import scalafx.collections.ObservableBuffer
+
+case class Message(message_id: String, date: String, is_from_me: Boolean, text: String)
+case class Subscriber(number: String, messages: List[Message])
+
 
 object main extends JFXApp3 {
   override def start(): Unit = {
     stage = new JFXApp3.PrimaryStage {
-      //    initStyle(StageStyle.Unified)
       title = "iToolsSmsViewer"
-      scene = new Scene {
-        fill = Color.rgb(38, 38, 38)
-        content = new HBox {
-          padding = Insets(50, 80, 50, 80)
 
-          children = Seq(
-            new Text {
-              text = fetchTextFromDatabase()
-              style = "-fx-font: normal bold 20pt sans-serif"
-              fill = new LinearGradient(
-                endX = 0,
-                stops = Stops(Red, DarkRed))
-            },
+      scene = new Scene(800, 570) {
+        var currentContact: Option[Subscriber] = None
 
-            new Text {
-              text = "!!!"
-              style = "-fx-font: italic bold 20pt sans-serif"
-              fill = new LinearGradient(
-                endX = 0,
-                stops = Stops(White, DarkGray)
-              )
-              effect = new DropShadow {
-                color = DarkGray
-                radius = 15
-                spread = 0.25
-              }
+        val contacts: ListView[Subscriber] = new ListView(fetchSubscribersFromDatabase()) {
+          cellFactory = TextFieldListCell.forListView(
+            new StringConverter[Subscriber] {
+              def toString(subscriber: Subscriber): String = subscriber.number
+
+              def fromString(string: String): Subscriber = ???
             }
           )
         }
+
+        val messages: ListView[Message] = new ListView[Message]() {
+          cellFactory = TextFieldListCell.forListView(
+            new StringConverter[Message] {
+              def toString(message: Message): String = s"${message.date}: ${message.text}"
+
+              def fromString(string: String): Message = ???
+            }
+          )
+        }
+
+        contacts.selectionModel().selectedItemProperty.onChange { (_, _, newSubscriber) =>
+          currentContact = Option(newSubscriber)
+          currentContact match {
+            case Some(subscriber) => messages.items = ObservableBuffer(subscriber.messages: _*)
+            case None => messages.items = ObservableBuffer()
+          }
+        }
+
+        // Display settings
+        messages.layoutX = 200
+
+        content = List(contacts, messages)
       }
     }
   }
 
-  def fetchTextFromDatabase(): String = {
+  def fetchSubscribersFromDatabase(): List[Subscriber] = {
     Class.forName("org.sqlite.JDBC")
-
     val connectionUrl = "jdbc:sqlite:./sms.db"
     val connection: Connection = DriverManager.getConnection(connectionUrl)
 
-    var result = ""
+    var subscribers = Map[String, Subscriber]()
+
     try {
       val statement = connection.createStatement()
-
       val queryGetAll =
         """
         SELECT chat.ROWID as subscriber_id,
@@ -77,24 +89,46 @@ object main extends JFXApp3 {
 
       val resultSet: ResultSet = statement.executeQuery(queryGetAll)
 
-      val stringBuilder = new StringBuilder
-
       while (resultSet.next()) {
-        val subscriberId = resultSet.getString("subscriber_id")
         val number = resultSet.getString("number")
         val messageId = resultSet.getString("message_id")
-        val date = resultSet.getString("date")
+        val date = convertDate(resultSet.getString("date"))
         val text = resultSet.getString("text")
-        val isFromMe = resultSet.getString("is_from_me")
+        val isFromMe = resultSet.getBoolean("is_from_me")
 
-        stringBuilder.append(s"Subscriber ID: $subscriberId, Number: $number, Message ID: $messageId, Date: $date, Text: $text, Is From Me: $isFromMe\n")
+        val message = Message(messageId, date, isFromMe, text)
+
+        subscribers.get(number) match {
+          case Some(subscriber) =>
+            val updatedSubscriber = subscriber.copy(messages = subscriber.messages :+ message)
+            subscribers = subscribers + (number -> updatedSubscriber)
+          case None =>
+            subscribers = subscribers + (number -> Subscriber(number, List(message)))
+        }
       }
-      result = stringBuilder.toString()
     } catch {
       case e: Exception => e.printStackTrace
     } finally {
       connection.close()
     }
-    result
+
+    subscribers.values.toList
+  }
+
+  def filterSubscribers(subscribers: List[Subscriber], search: String): List[Subscriber] = {
+    subscribers.filter { subscriber =>
+      subscriber.messages.exists { message =>
+        message.date.contains(search) || message.text.contains(search)
+      }
+    }
+  }
+
+  def convertDate(date: String): String = {
+    val origin = LocalDateTime.of(2000, 12, 26, 4, 0, 0)
+    val zoneId = ZoneId.of("UTC")
+    val zonedOrigin = origin.atZone(zoneId)
+    val duration = Duration.ofSeconds(date.toLong)
+    val target = zonedOrigin.plus(duration)
+    DateTimeFormatter.RFC_1123_DATE_TIME.format(target)
   }
 }
